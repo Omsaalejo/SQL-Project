0b50630f42fda494d2eef19036f378b2c228f82c
HerParra12 <halvaradop@unbosque.edu.co>
2022-05-15 23:36:45 -0500

    sql mkdir

---
+++ SQL/create_table.sql
@@ -0,0 +1,36 @@
+/*
+CREATE TABLE Person_User(
+                            name VARCHAR(30) NOT NULL,
+                            email VARCHAR(20) NOT NULL,
+                            password VARCHAR(15) NOT NULL,
+                            description VARCHAR(40),
+                            role VARCHAR(10),
+                            id_person INT,
+                            fcoins float
+);
+
+CREATE TABLE Artist(
+                       description VARCHAR(40),
+                       id_person INT
+);
+
+CREATE TABLE Buyer(
+                      fcoins float,
+                      id_person INT
+);
+
+CREATE TABLE Part(
+                     title VARCHAR(20),
+                     id_collection INT,
+                     categori_part VARCHAR(15),
+                     description VARCHAR(30),
+                     fcoins float
+);
+
+CREATE TABLE Collection(
+                           id_collection INT,
+                           title VARCHAR(20),
+                           picture VARCHAR,
+                           description VARCHAR(30)
+);
+*/
---
+++ SQL/insert.sql
@@ -0,0 +1,12 @@
+/*
+INSERT INTO Person_User (name, email, password, role) VALUES ('first', 'first@gmail.com', '1234', 'Vendedor');
+INSERT INTO Person_User (name, email, password, role) VALUES ('second', 'second@gmail.com', '1235', 'Vendedor');
+INSERT INTO Person_User (name, email, password, role) VALUES ('third', 'third@gmail.com', '1236', 'Comprador');
+INSERT INTO Person_User (name, email, password, role) VALUES ('four', 'four@gmail.com', '1237', 'Comprador');
+
+INSERT INTO Person_User (name, email, password, role) VALUES ('juan', 'juan@gmail.com', '1238', 'Vendedor');
+INSERT INTO Person_User (name, email, password, role) VALUES ('camilo', 'camilo@gmail.com', '1239', 'Vendedor');
+INSERT INTO Person_User (name, email, password, role) VALUES ('jose', 'jose@gmail.com', '1240', 'Comprador');
+INSERT INTO Person_User (name, email, password, role) VALUES ('andres', 'andres@gmail.com', '1241', 'Comprador');
+
+*/
